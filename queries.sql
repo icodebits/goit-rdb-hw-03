e318f09fe642c0f019a2edd83dd9e029728d64bc
Serghei Filimonov <filimonov.sergey@gmail.com>
2024-10-21 19:16:41 +0300

    Add p2.

--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /* p1 */
 SELECT * FROM mydb.products;
 SELECT name, phone FROM mydb.shippers;
+
+/* p2 */
+SELECT 
+    AVG(price) AS average_price, 
+    MAX(price) AS max_price, 
+    MIN(price) AS min_price
+FROM mydb.products;
